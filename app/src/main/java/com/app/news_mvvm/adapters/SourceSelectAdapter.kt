36e0927aaffb74ee46bf2a70eb9549d98cd8bc51
Akash Gandhi <gandhiakash8111@gmail.com>
2021-01-20 17:22:39 +0530

    Added DiffUtil class in SourceSelectAdapter

--- app/src/main/java/com/app/news_mvvm/adapters/SourceSelectAdapter.kt
+++ app/src/main/java/com/app/news_mvvm/adapters/SourceSelectAdapter.kt
@@ -3,26 +3,39 @@ package com.app.news_mvvm.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.app.news_mvvm.R
 import com.app.news_mvvm.model.SourceSelect
 import com.google.android.material.checkbox.MaterialCheckBox
 
-class SourceSelectAdapter(private var sourceList: List<SourceSelect> ,
-                          private var listener: SourceCheckedListener) :
+class SourceSelectAdapter(private var listener: SourceCheckedListener) :
         RecyclerView.Adapter<SourceSelectAdapter.SourceHolder>() {
-
+/*
     fun updateSourceSelectList(sourceList: List<SourceSelect>) {
         this.sourceList = sourceList
         notifyDataSetChanged()
+    }*/
+
+    private val diffUtil = object : DiffUtil.ItemCallback<SourceSelect>() {
+        override fun areItemsTheSame(oldItem: SourceSelect, newItem: SourceSelect): Boolean {
+            return oldItem.sourceName == newItem.sourceName
+        }
+
+        override fun areContentsTheSame(oldItem: SourceSelect, newItem: SourceSelect): Boolean {
+            return oldItem == newItem
+        }
     }
+
+    val diffList = AsyncListDiffer(this , diffUtil)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SourceSelectAdapter.SourceHolder {
        return SourceHolder(LayoutInflater.from(parent.context).inflate(R.layout.source_select_row , parent , false))
     }
 
     override fun onBindViewHolder(holder: SourceSelectAdapter.SourceHolder, position: Int) {
-        holder.bindData(sourceList[position])
+        holder.bindData(diffList.currentList[position])
     }
 
     override fun getItemId(position: Int): Long {
@@ -32,7 +45,7 @@ class SourceSelectAdapter(private var sourceList: List<SourceSelect> ,
     override fun getItemViewType(position: Int): Int {
         return position
     }
-    override fun getItemCount(): Int = sourceList.size
+    override fun getItemCount(): Int = diffList.currentList.size
 
     inner class SourceHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         private val sourceSelector :MaterialCheckBox = itemView.findViewById(R.id.view_checkedB)
@@ -40,9 +53,10 @@ class SourceSelectAdapter(private var sourceList: List<SourceSelect> ,
         init {
             sourceSelector.setOnCheckedChangeListener { _, isChecked ->
 
-                sourceList[adapterPosition].isSelected = isChecked
-                listener.onSourceChecked(sourceList[adapterPosition] , adapterPosition)
-
+               // sourceList[adapterPosition].isSelected = isChecked
+                diffList.currentList[absoluteAdapterPosition].isSelected = isChecked
+                //listener.onSourceChecked(sourceList[adapterPosition] , adapterPosition)
+                listener.onSourceChecked(diffList.currentList[absoluteAdapterPosition] , absoluteAdapterPosition)
 
             }
         }
